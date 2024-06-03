62efca0939d31746532529cda7b866ccd3a7b34f
SaiShivani91 <msaishivani1031@gmail.com>
2024-06-02 20:37:43 -0400

    Hyperparamater Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -42,12 +42,50 @@ class ModelTrainer:
                 "AdaBoost": AdaBoostRegressor(),
                 "XGBoost": XGBRegressor(),
                 "GradientBoost": GradientBoostingRegressor(),
-                "CatBoost": CatBoostRegressor(),
-                "SVM": SVR()
+                "CatBoost": CatBoostRegressor()
+                
+            }
+
+            params = {
+                 "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "GradientBoost":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBoost":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoost":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
             }
 
             model_report: dict = evaluate_models(X_train = X_train, Y_train = Y_train, X_test = X_test, 
-                                                 Y_test = Y_test, models = models)
+                                                 Y_test = Y_test, models = models, params = params)
             
             ## To get best model score out of all
 
